8dfa98d0635e7a6aed7065de76a260fba6139487
gabriele m <gabriele.marazzi@gmail.com>
2024-11-16 18:12:36 +0700

    Add ChallengeCardInfo

---
+++ dongle-front/src/components/ChallengeCardInfo.tsx
@@ -0,0 +1,13 @@
+// ChallengeCardInfo
+
+
+const ChallengeCardInfo = ({ challenge }: any) => {
+      
+    return (
+        <div className="bg-white p-1 text-black rounded-md shadow-sm">
+        Card.. INFO
+      </div>
+    )
+  }
+
+  export default ChallengeCardInfo;
