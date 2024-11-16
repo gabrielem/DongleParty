df72685e2514adc000ebcc4490b4c637e98e1fa1
gabriele m <gabriele.marazzi@gmail.com>
2024-11-16 22:22:38 +0700

    Add check

--- dongle-front/src/pages/api/getBalances.ts
+++ dongle-front/src/pages/api/getBalances.ts
@@ -6,10 +6,12 @@ async function getBalancesHandler(req: any, res: any) {
 
     // arbitrum: 42161; || base: 8453
 
-    const {chain, address} = req?.body 
-    if (!chain || !address) {
-        return res.status(400).json({ error: 'Missing required fields, chain, and address are required' });
-    }
+    // const {chain, address} = req?.body 
+    // if (!chain || !address) {
+    //     return res.status(400).json({ error: 'Missing required fields, chain, and address are required' });
+    // }
+    const chain = req.query.chain || '42161';
+    const address = req.query.address || '0xFB3bc2F5EDB94516BcbFaD47931BF04a057d0e70'
 
     const url = `https://api.1inch.dev/balance/v1.2/${chain}/balances/${address}`;
     const config = {
