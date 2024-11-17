896bca914be087560efbcbcd098256c511f5ae09
zlace <zlace0x@gmail.com>
2024-11-17 08:13:59 +0700

    fix: types

--- dongle-front/src/utils/utils.ts
+++ dongle-front/src/utils/utils.ts
@@ -1,15 +1,15 @@
 import { formatDistanceToNow, format } from "date-fns";
 
 export const validateEmail = (email: string): string | null => {
-    if (!email || !email.trim()) {
-      return 'L\'indirizzo email non può essere vuoto.'
-    }
-    const emailPattern: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-  
-    if (!emailPattern.test(email)) {
-      return 'L\'indirizzo email non è valido.'
-    }
-    return null
+  if (!email || !email.trim()) {
+    return 'L\'indirizzo email non può essere vuoto.'
+  }
+  const emailPattern: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+  if (!emailPattern.test(email)) {
+    return 'L\'indirizzo email non è valido.'
+  }
+  return null
 }
 
 export const formatBalance = (balance: string, decimals: number): string => {
@@ -18,31 +18,31 @@ export const formatBalance = (balance: string, decimals: number): string => {
 };
 
 export const getErrorMessage = (error: any) => {
-    // console.log('❌❌❌ getErrorMessage', error)
-    
-    if (error && (typeof error === 'string' || error instanceof String)) {
-        return error
-    } else if (error?.response?.data?.error?.message) {
-        return error.response.data.error.message
-    } else if (error?.response?.data?.error?.detail) {
-        return error.response.data.error.detail
-    } else if (error?.response?.data?.error) {
-        return error.response.data.error
-    } else if (error?.response?.data?.message) {
-        return error.response.data.message
-    } else if (error?.request?.statusText) {
-        return error.request.statusText
-    } else if (error?.request?.status) {
-        return error.request.status
-    } else if (error?.message) {
-        return error.message
-    } else {
-        return "Server error"
-    }
+  // console.log('❌❌❌ getErrorMessage', error)
+
+  if (error && (typeof error === 'string' || error instanceof String)) {
+    return error
+  } else if (error?.response?.data?.error?.message) {
+    return error.response.data.error.message
+  } else if (error?.response?.data?.error?.detail) {
+    return error.response.data.error.detail
+  } else if (error?.response?.data?.error) {
+    return error.response.data.error
+  } else if (error?.response?.data?.message) {
+    return error.response.data.message
+  } else if (error?.request?.statusText) {
+    return error.request.statusText
+  } else if (error?.request?.status) {
+    return error.request.status
+  } else if (error?.message) {
+    return error.message
+  } else {
+    return "Server error"
   }
+}
 
 
-export const formatTimestampForMessages = (timestamp: number, locale = 'en') => {  
+export const formatTimestampForMessages = (timestamp: number | string | Date, locale = 'en') => {
   const now = new Date();
   const date = new Date(timestamp);
 
